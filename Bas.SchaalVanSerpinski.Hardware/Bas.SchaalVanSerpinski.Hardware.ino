edb0783b0f59701a576070ed98a4ff48b2911cad
Bas Paap <baspaap@live.nl>
2022-05-28 16:51:52 +0200

    Set up Visual Micro project.

---
+++ Bas.SchaalVanSerpinski.Hardware/Bas.SchaalVanSerpinski.Hardware.ino
@@ -0,0 +1,24 @@
+
+int ledPins[] = { 3, 5, 6, 9, 10 };
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+	for (size_t i = 0; i < sizeof(ledPins); i++)
+	{
+		pinMode(ledPins[i], OUTPUT);
+	}
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+	for (size_t i = 0; i < sizeof(ledPins); i++)
+	{
+		for (int fadeValue = 0; fadeValue <= 255; fadeValue++)
+		{
+			analogWrite(ledPins[i], fadeValue);
+			delay(1);
+		}
+	}
+}
