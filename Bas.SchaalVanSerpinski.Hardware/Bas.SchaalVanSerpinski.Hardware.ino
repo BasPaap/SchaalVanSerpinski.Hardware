7a1502d0281309e53f79522d2df8a96456cff31a
Bas Paap <baspaap@live.nl>
2022-05-30 23:25:37 +0200

    Add PentagramEnd effect.

--- Bas.SchaalVanSerpinski.Hardware/Bas.SchaalVanSerpinski.Hardware.ino
+++ Bas.SchaalVanSerpinski.Hardware/Bas.SchaalVanSerpinski.Hardware.ino
@@ -10,6 +10,7 @@ int pentagramDisplayDuration = 5000;
 constexpr auto handshakeCommand = "SERPINSKI";
 constexpr auto handshakeResponse = "SERPINSKI_OK";
 constexpr auto showPentagramStartCommand = "PENTAGRAM_START";
+constexpr auto showPentagramEndCommand = "PENTAGRAM_END";
 
 Bas::CommandHandler commandHandler;
 
@@ -23,13 +24,13 @@ void setup()
 
 	Serial.println("Schaal van Serpinski hardware controller v1.0");
 
-	for (size_t i = 0; i < sizeof(ledPins)/sizeof(int); i++)
+	for (size_t i = 0; i < sizeof(ledPins) / sizeof(int); i++)
 	{
 		pinMode(ledPins[i], OUTPUT);
 	}
 
 	// Listen for the handshake command. If we receive it, enable all other commands.
-	commandHandler.setCallback(handshakeCommand, enableSerialCommands);	
+	commandHandler.setCallback(handshakeCommand, enableSerialCommands);
 }
 
 // Add the main program code into the continuous loop() function
@@ -63,10 +64,39 @@ void showPentagramStart()
 	}
 }
 
+void showPentagramEnd()
+{
+	int numLeds = sizeof(ledPins) / sizeof(int);
+
+	for (int fadeInValue = 0; fadeInValue <= fadeInMaxValue; fadeInValue++)
+	{
+		for (size_t i = 0; i < numLeds; i++)
+		{
+			analogWrite(ledPins[i], fadeInValue);
+		}
+
+		delay(fadeInSpeed);
+	}
+
+
+	delay(pentagramDisplayDuration);
+
+	for (int glowOutValue = fadeInMaxValue; glowOutValue >= 0; glowOutValue--)
+	{
+		for (size_t i = 0; i < numLeds; i++)
+		{
+			analogWrite(ledPins[i], glowOutValue);
+		}
+
+		delay(glowSpeed);
+	}
+}
+
 void enableSerialCommands()
 {
 	// We received a request to start sending commands from the host device,
 	// so we'll enable all commands and respond that we are ready to receive them.
 	commandHandler.setCallback(showPentagramStartCommand, showPentagramStart);
+	commandHandler.setCallback(showPentagramEndCommand, showPentagramEnd);
 	Serial.println(handshakeResponse);
 }
